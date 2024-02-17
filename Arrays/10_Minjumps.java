c4554728397324d2f77efcd874dd9cb22cc139ea
Reguvel <111241085+reguvel@users.noreply.github.com>
2024-02-17 23:29:47 +0530

    Create 10_Minjumps.java

---
+++ Arrays/10_Minjumps.java
@@ -0,0 +1,59 @@
+//link:https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+class Solution{
+    static int minJumps(int arr[])
+    {
+        if (arr.length <= 1)
+            return 0;
+       
+          //If value of first index guarantees 
+          //only 1 jump is needed, return 1
+        if (arr[0] >= arr.length-1)
+            return 1;
+ 
+        // Return -1 if not possible to jump
+        if (arr[0] == 0)
+            return -1;
+ 
+        // initialization
+        int maxReach = arr[0];
+        int step = arr[0];
+        int jump = 1;
+ 
+        // Start traversing array
+        for (int i = 1; i < arr.length; i++) {
+            // Check if we have reached 
+// the end of the array
+            if (i == arr.length - 1)
+                return jump;
+           
+          //Check if value at current index guarantees jump to end
+            if (arr[i] >= (arr.length-1) - i)
+                return jump + 1;
+ 
+            // updating maxReach
+            maxReach = Math.max(maxReach, i + arr[i]);
+ 
+            // we use a step to get to the current index
+            step--;
+ 
+            // If no further steps left
+            if (step == 0) {
+                // we must have used a jump
+                jump++;
+ 
+                // Check if the current 
+// index/position or lesser index
+                // is the maximum reach point 
+// from the previous indexes
+                if (i >= maxReach)
+                    return -1;
+ 
+                // re-initialize the steps to the amount
+                // of steps to reach maxReach from position i.
+                step = maxReach - i;
+            }
+        }
+ 
+        return -1;
+    }
+}
